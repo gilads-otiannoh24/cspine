8ad2dca0511752de3fc2cc67229168e2315da1b0
gilads_otiannoh254 <ochiengian383@gmail.com>
2025-06-14 01:27:48 +0300

    `Update dependencies and plugins in client module`

--- client/module.js
+++ client/module.js
@@ -1,5 +1,7 @@
 import utils from "../node_modules/@gilads-otiannoh24/alpine-csp-utils/dist/utils.esm.js";
 import Alpine from "../node_modules/alpinejs/dist/module.esm.js";
 
+window.Alpine = Alpine;
+
 Alpine.plugin(utils);
 Alpine.start();
